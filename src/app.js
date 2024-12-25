b861f1f0dd5641ac7370a5f1130cd4b21e2991fc
RaniAagalave <raniaagalave47@gmail.com>
2024-12-25 19:07:33 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit :"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+//routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users",userRouter)
 
 export { app };
